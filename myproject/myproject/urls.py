2cf6079b4187fd89c0d0f57cbeb3506b66f27e5d
Varshu2806 <varshiniavvari09@gmail.com>
2025-06-30 15:50:18 +0530

    Update urls.py

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
+    path('', lambda request: HttpResponse("✅ Welcome to MyProject API. Visit /api/ for API routes.")),
 ]
