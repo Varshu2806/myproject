a2fb315914adfaeec2d774669a6f1d3ecea3fb99
Varshu2806 <varshiniavvari09@gmail.com>
2025-06-30 16:21:36 +0530

    Update urls.py

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -1,17 +1,27 @@
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.urls import path
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        "register": "/api/register/",
+        "login": "/api/login/",
+        "profile": "/api/profile/",
+        "send-otp": "/api/send-otp/",
+        "users": "/api/users/"
+    })
+
 from .views import (
     RegisterView,
     LoginView,
     UserProfileView,
     UserListView,
     SendOTPView,
-    
-
 )
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
+    path('', api_root, name='api-root'),
     path('register/', RegisterView.as_view(), name='register'),
     path('users/', UserListView.as_view(), name='user-list'),
     path('login/', LoginView.as_view(), name='login'),
